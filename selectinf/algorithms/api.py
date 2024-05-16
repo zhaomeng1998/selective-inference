2b98b5591ad36582665fca62469f94452d4079c9
zhaomeng1998 <96006902+zhaomeng1998@users.noreply.github.com>
2024-05-15 21:40:07 -0700

    Update api.py

--- selectinf/algorithms/api.py
+++ selectinf/algorithms/api.py
@@ -1,5 +1,5 @@
 from .lasso import (lasso, 
-                    lasso_ols
+                    lasso_ols,
                     ROSI,
                     data_carving as data_carving_lasso, 
                     additive_noise as additive_noise_lasso)
