b011bdacf6c96bf1a945b064f1d43c3b8d46a8a2
zhaomeng1998 <96006902+zhaomeng1998@users.noreply.github.com>
2024-05-15 21:47:09 -0700

    Update api.py

--- selectinf/algorithms/api.py
+++ selectinf/algorithms/api.py
@@ -3,7 +3,7 @@ from .lasso import (lasso,
                     data_carving as data_carving_lasso, 
                     additive_noise as additive_noise_lasso)
 
-from .lasso_ols import lasso as lasso_new
+from .lasso_ols import (lasso_new)
 
 from .sqrt_lasso import (choose_lambda as choose_lambda_sqrt_lasso, 
                          solve_sqrt_lasso)
